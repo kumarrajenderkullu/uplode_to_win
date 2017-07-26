38b49582b8b51549d955ac08eafb112d858fcd62
kumarrajenderkullu <kumarrajenderkullu@gmail.com>
2017-07-26 21:52:46 +0530

    Latest Commit

--- upload_to_win/App/models.py
+++ upload_to_win/App/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=255)
+    #password = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
--- upload_to_win/upload_to_win/settings.py
+++ upload_to_win/upload_to_win/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'App'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'upload_to_win.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'upload_to_win',
+        'USER': 'MySQL',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT':'3306',
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'IND'
 
 USE_I18N = True
 
